ebc414f16f6047246f473c73d23f94f420bbc021
Steven Bazyl <sqrrrl@gmail.com>
2012-10-26 13:25:00 -0700

    Update drive sample based on quickstart guide

---
+++ drive/README.md
@@ -0,0 +1,93 @@
+# Quickstart: Run a Drive App
+
+Complete the steps described in the rest of this page, and in less than five
+minutes you'll have a simple Drive app that uploads a file to Google Drive.
+
+## Enable the Drive API
+
+First, you need to enable the Drive API for your app. You can do this in your
+app's API project in the [Google APIs
+Console](https://code.google.com/apis/console/).
+
+1. Create an API project in the [Google APIs
+   Console](https://code.google.com/apis/console/).
+2. Select the **Services** tab in your API project, and enable the Drive API.
+3. Select the **API Access** tab in your API project, and click **Create an
+   OAuth 2.0 client ID**.
+4. In the **Branding Information** section, provide a name for your application
+   (e.g. "Drive Quickstart Sample"), and click **Next**.  Providing a product
+   logo is optional.
+5. In the **Client ID Settings** section, do the following:
+      1. Select **Installed application** for the **Application type**
+         (or **Web application** for the JavaScript sample).
+      2. Select **Other** for the **Installed application type**.
+      3. Click **Create Client ID**.
+6. In the **API Access** page, locate the section **Client ID for installed
+   applications** and note or copy the two pieces of information you'll need
+   later to run the sample: the **Client ID** and the **Client Secret**
+
+## Install the Google Client Library
+
+To run the quickstart sample, you'll need to install the Google API client
+library.
+
+    gem install google-api-client
+    
+
+## Set up the sample
+
+You'll need to create two files: a test file to upload to Drive (in this case
+`document.txt`), and the given sample code, modified to include the unique
+**Client ID** and **Client Secret** you created in the "Enable the Drive API"
+step.
+
+
+1. Create a text file named `document.txt`, containing the text `Hello world!`.
+2. After following the directions below to copy source code, replace
+   `YOUR_CLIENT_ID` with the **Client ID** you generated in the "Enable the
+   Drive API" step.
+3. Replace `YOUR_CLIENT_SECRET` with your **Client Secret**, also generated
+   previously.
+
+
+## Run the Sample
+
+After you have set up your Google API project, installed the Google API client
+library, and set up the sample source code, the sample is ready to run.  The
+command-line samples provide a link you'll need to visit in order to
+authorize the sample.
+
+    ruby quickstart.rb
+
+1. Browse to the provided URL in your web browser.
+2. If you are not already logged into your Google account, you will be prompted
+   to log in.  If you are logged into multiple Google accounts, you will be
+   asked to select one account to use for the authorization.
+3. Copy the code you're given after browsing to the link, and paste it into the 
+   prompt `Enter authorization code:`.  Click **Enter**.
+
+When you finish these steps, `document.txt` is now stored in Google Drive.
+The command-line samples print Information about the Google Drive file to the screen.
+The file `document.txt` is accessible in Google Drive, and is titled "My
+Document".
+
+By editing the sample code to provide paths to new files and new titles,
+you can run a few more simple upload tests. When you're ready, you
+could try running some other Drive API methods such as
+[files.list](http://developers.google.com/drive/v2/reference/files/list).
+
+## Next Steps
+
+If your goal is to let users create and open files directly from the Drive UI
+using your app, see [Integrate with the Drive UI](https://developers.google.com/drive/enable-sdk).
+Our end-to-end [Example Apps](https://developers.google.com/drive/examples/index) demonstrate a simple
+Drive UI-integrated web app.
+
+If your goal is to expand the quickstart sample into something for your own
+installed application, consult the [API Reference](https://developers.google.com/drive/v2/reference). The
+API Reference discusses all of the features of the Drive API, and gives
+samples in each language on how to use a feature.
+
+All requests to the Drive API must be authorized by an authenticated user.
+To examine more authorization code and learn how to authorize requests,
+see [Retrieve and Use Credentials](https://developers.google.com/drive/credentials).
--- drive/drive.rb
+++ drive/drive.rb
@@ -11,221 +11,49 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-require 'sinatra'
+require 'rubygems'
 require 'google/api_client'
-require 'google/api_client/client_secrets'
-require 'logger'
-require 'multi_json'
-require 'data_mapper'
+require 'launchy'
 
-enable :sessions
+# Get your credentials from the APIs Console
+CLIENT_ID = 'YOUR_CLIENT_ID'
+CLIENT_SECRET = 'YOUR_CLIENT_SECRET'
+OAUTH_SCOPE = 'https://www.googleapis.com/auth/drive'
+REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 
-SCOPES = [
-    'https://www.googleapis.com/auth/drive.file',
-    'https://www.googleapis.com/auth/userinfo.email'
-]
+# Create a new API client & load the Google Drive API 
+client = Google::APIClient.new
+drive = client.discovered_api('drive', 'v2')
 
-##
-# Datastore entity for storing OAuth 2.0 credentials.
-class User
-  include DataMapper::Resource
-  property :id, Serial
-  property :email, String, :length => 255
-  property :refresh_token, String, :length => 255
-end
+# Request authorization
+client.authorization.client_id = CLIENT_ID
+client.authorization.client_secret = CLIENT_SECRET
+client.authorization.scope = OAUTH_SCOPE
+client.authorization.redirect_uri = REDIRECT_URI
 
-##
-# Set up
-configure do
-  db_url = Addressable::URI.parse(
-    ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/user.db"
-  )
-  DataMapper.setup(:default, db_url)
-  User.auto_migrate!
+uri = client.authorization.authorization_uri
+Launchy.open(uri)
 
-  # App identity
-  set :credentials, Google::APIClient::ClientSecrets.load
-  set :app_id, settings.credentials.client_id.sub(/[.-].*/,'')
+# Exchange authorization code for access token
+$stdout.write  "Enter authorization code: "
+client.authorization.code = gets.chomp
+client.authorization.fetch_access_token!
 
-  # Preload API definitions
-  client = Google::APIClient.new
-  set :drive, client.discovered_api('drive', 'v1')
-  set :oauth2, client.discovered_api('oauth2', 'v2')
-end
+# Insert a file
+file = drive.files.insert.request_schema.new({
+  'title' => 'My document',
+  'description' => 'A test document',
+  'mimeType' => 'text/plain'
+})
 
-helpers do
-  ##
-  # Render json data in a response
-  def json(data)
-    content_type :json
-    [200, data.to_json]
-  end
+media = Google::APIClient::UploadIO.new('document.txt', 'text/plain')
+result = client.execute(
+  :api_method => drive.files.insert,
+  :body_object => file,
+  :media => media,
+  :parameters => {
+    'uploadType' => 'multipart',
+    'alt' => 'json'})
 
-  ##
-  # Get an API client instance
-  def api_client
-    @client ||= (begin
-      client = Google::APIClient.new
-
-      client.authorization.client_id = settings.credentials.client_id
-      client.authorization.client_secret = settings.credentials.client_secret
-      client.authorization.redirect_uri =
-        settings.credentials.redirect_uris.first
-      client.authorization.scope = SCOPES
-      client
-    end)
-  end
-
-  def current_user
-    if session[:user_id]
-      @user ||= User.get(session[:user_id])
-    end
-  end
-
-  def authorized?
-    return (
-      api_client.authorization.refresh_token &&
-      api_client.authorization.access_token
-    )
-  end
-end
-
-before do
-  # Make sure access token is up to date for each request
-  api_client.authorization.update_token!(session)
-  if api_client.authorization.refresh_token &&
-      api_client.authorization.expired?
-    api_client.authorization.fetch_access_token!
-  end
-end
-
-after do
-  # Serialize the access/refresh token to the session
-  session[:access_token] = api_client.authorization.access_token
-  session[:refresh_token] = api_client.authorization.refresh_token
-  session[:expires_in] = api_client.authorization.expires_in
-  session[:issued_at] = api_client.authorization.issued_at
-end
-
-##
-# Upgrade our authorization code when a user launches the app from Drive &
-# ensures saved refresh token is up to date
-def authorize_code(authorization_code)
-  api_client.authorization.code = authorization_code
-  api_client.authorization.fetch_access_token!
-
-  result = api_client.execute!(:api_method => settings.oauth2.userinfo.get)
-  user = User.first_or_create(:email => result.data.email)
-  api_client.authorization.refresh_token = (
-    api_client.authorization.refresh_token || user.refresh_token
-  )
-  if user.refresh_token != api_client.authorization.refresh_token
-    user.refresh_token = api_client.authorization.refresh_token
-    user.save
-  end
-  session[:user_id] = user.id
-end
-
-def auth_url(state = '')
-  user_email = current_user ? current_user.email : ''
-  return api_client.authorization.authorization_uri(
-    :state => state,
-    :approval_prompt => :force,
-    :access_type => :offline,
-    :user_id => user_email
-  ).to_s
-end
-
-##
-# Prepare request data for upload
-def prepare_data(body)
-  data = MultiJson.decode(body)
-  content = StringIO.new(data['content'])
-  resource_id = data['resource_id']
-
-  data.keep_if { |k,v| %w{title description mimeType}.include? k}
-  file_content = Google::APIClient::UploadIO.new(content, data['mimeType'])
-
-  [resource_id, data, file_content]
-end
-
-##
-# Main entry point for the app. Ensures the user is authorized &
-# inits the editor for either edit of the opened files or creating a new file.
-get '/' do
-  state = params[:state] || '{}'
-  if params[:code]
-    authorize_code(params[:code])
-    redirect to("/?state=#{Addressable::URI.encode(state)}")
-  elsif params[:error] # User denied the oauth grant
-    halt 403
-  end
-
-  redirect auth_url(state) unless authorized?
-
-  state = MultiJson.decode(state)
-  @state = state
-  @file_ids = (state['ids'] || [''])
-  @app_id = settings.app_id
-  erb :index
-end
-
-###
-# Load content
-#
-get '/svc' do
-  result = api_client.execute!(
-    :api_method => settings.drive.files.get,
-    :parameters => { 'id' => params['file_id'] })
-  file = result.data.to_hash
-  result = api_client.execute!(:uri => result.data.download_url)
-  file['content'] = result.body
-  json file
-end
-
-##
-# Save a new file
-post '/svc' do
-  _, file, content = prepare_data(request.body)
-  result = api_client.execute!(
-    :api_method => settings.drive.files.insert,
-    :body_object => file,
-    :media => content,
-    :parameters => {
-      'uploadType' => 'multipart',
-      'alt' => 'json'})
-  json result.data.id
-end
-
-##
-# Update existing file
-put '/svc' do
-  resource_id, file, content = prepare_data(request.body)
-  result = api_client.execute!(
-    :api_method => settings.drive.files.update,
-    :body_object => file,
-    :media => content,
-    :parameters => {
-      'id' => resource_id,
-      'newRevision' => "true",
-      'uploadType' => 'multipart',
-      'alt' => 'json'})
-  json result.data.id
-end
-
-##
-# For /svc methods, assume client errors are due to auth failures
-# and return a redirect (via JSON)
-error Google::APIClient::ClientError do
-  response = {
-    'redirect' => auth_url('{}')
-  }
-  json response
-end
-
-##
-# Oh noes!
-error Google::APIClient::ServerError do
-  halt 500
-end
+# Pretty print the API result
+jj result.data.to_hash
