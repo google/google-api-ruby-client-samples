29eacf659d171dfadb811460841741bb0004d662
Jonathon Imperiosi <jimper@google.com>
2013-11-21 09:48:48 -0500

    Adding copyright and comments. Removing debugging code.

--- dfareporting/dfareporting_utils.rb
+++ dfareporting/dfareporting_utils.rb
@@ -1,3 +1,17 @@
+# Copyright (C) 2013 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require 'rubygems'
 require 'google/api_client'
 require 'google/api_client/client_secrets'
@@ -47,8 +61,6 @@ class DfaReportingUtils
   
     client = Google::APIClient.new(:application_name => 'Ruby DFA Reporting sample',
                                    :application_version => '1.0.0')
-  
-    # client.logger.level = Logger::DEBUG
     
     # FileStorage stores auth credentials in a file, so they survive multiple runs
     # of the application. This avoids prompting the user for authorization every
