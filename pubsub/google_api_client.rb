bdb6007588eaedd316bfd1a62826a4bc8878473c
Tim Uckun <tim@uckun.com>
2014-08-25 09:26:44 +1200

    added pubsub samples. Needs some cleanup but works

---
+++ pubsub/google_api_client.rb
@@ -0,0 +1,137 @@
+class GoogleApiClient
+
+
+  def initialize(opts={})
+    @authorization = nil
+    @api = nil
+    #these will be injected into this object as attr_accessor
+    @opts          =HashParams.new(opts) do
+      #mandatory
+      param :application_name, required: true
+      param :application_version, required: true
+
+      param :log_level, default: Logger::Severity::WARN
+      #optional
+      param :cache_timeout_in_seconds, coerse: Integer, :default => 60 * 60
+      param :cache_directory, default: '/tmp'
+      param :debug, default: false
+
+      #these can be passed in in order to save the extra call to authorize
+      param :scope, default: 'https://www.googleapis.com/auth/cloud-platform'
+      param :service_email
+      param :key_path
+      #if these are passed in it will automatically discover the api
+      param :api_name
+      param :api_version
+    end
+
+
+    @raw_client = Google::APIClient.new(
+        application_name:    @opts[:application_name],
+        application_version: @opts[:application_version]
+    )
+
+    @raw_client.logger.level = Logger::Severity::DEBUG
+    #if they passed in authorisation opts then do it
+    if @opts[:scope] && @opts[:service_email] && @opts[:key_path]
+      authorise @opts[:scope], @opts[:service_email], @opts[:key_path] , true
+    end
+
+    binding.pry
+    #if api is specified then discover it
+    if @opts[:api_name] && @opts[:api_version]
+      discover(@opts[:api_name] ,  @opts[:api_version])
+    end
+  end
+
+  def authorised?
+    @authorization.nil?
+  end
+
+  alias_method :authorized?, :authorised?
+
+  def authorise(scope=@opts[:scope], service_email=@opts[:service_email], key_path=@opts[:key_path], force = false)
+    #memoize unless forced
+    return @authorization if (@authorization  && !force )
+    key = Google::APIClient::PKCS12.load_key(File.open(key_path, mode: 'rb'), 'notasecret')
+
+    asserter       = Google::APIClient::JWTAsserter.new(
+        service_email,
+        scope,
+        key
+    )
+    @authorization = @raw_client.authorization = asserter.authorize
+  end
+
+  alias_method :authorize, :authorise
+
+  def discover(api, version, cache_timeout=nil)
+    cache_timeout ||= @opts[:cache_timeout_in_seconds]
+    filename      = File.join(@opts[:cache_directory], "discovered_google_api_#{api}_#{version}.json")
+
+    cache_valid     = File.exists?(filename) && (Time.now - File.ctime(filename)) < cache_timeout
+    cached_document = File.read(filename) if cache_valid
+    #   this will register a previously discovered document with the client
+    #    it eliminates a needless http request
+    @raw_client.register_discovery_document(api, version, cached_document) if cached_document
+    #   #this call will only initiatiate an http response if the discovery document is missing
+    @api = @raw_client.discovered_api(api, version)
+    #
+    #   #if there was no cached document write one.
+    File.write(filename, @api.discovery_document.to_json) unless cache_valid
+
+    # return the discovered_api
+    @api
+  end
+
+  def cached_call(cache_key, cache_timeout=nil)
+    cache_timeout ||= @opts[:cache_timeout_in_seconds]
+    filename      = File.join(@opts[:cache_directory].to_s, "#{@opts[:application_name].to_s}_#{@opts[:application_version].to_s}_#{cache_key.to_s}.cache.marshal")
+    cache_valid   = File.exists?(filename) && (Time.now - File.ctime(filename)) < cache_timeout
+    if cache_valid
+      Marshal.load(File.read(filename))
+    else
+      result = yield(self) if block_given?
+      File.write(filename, Marshal.dump(result))
+      result
+    end
+  end
+
+  # Generic API call
+  def execute(api_method, parameters={}, body_object ={}, opts={})
+
+
+    method= @api.instance_eval(api_method)
+
+    #project id is not required for all calls to all APIs but it is required for a ton of them
+    #this  is a convinient way to put it put it in
+
+    project_id              = parameters.delete(:projectId) ||
+        parameters.delete('projectId') ||
+        parameters.delete(:project_id) ||
+        parameters.delete('project_id') ||
+        @opts[:project_id]
+
+    parameters['projectId'] = project_id if project_id
+
+    h               = {:api_method => method, :parameters => parameters}
+    h[:body_object] = body_object unless body_object.empty?
+    h.merge.opts unless opts.empty?
+    raw_execute h
+  end
+  alias_method :call, :execute
+
+  #delegated methods to @raw_client
+  def raw_execute(*params)
+    r=@raw_client.execute(*params)
+    raise r.error_message if r.error?
+
+    #Not sure if this is a special case or not but better to be more specific
+    empty_body_returns=['pubsub.topics.publish', 'pubsub.subscriptions.acknowledge']
+    return true if empty_body_returns.include?(r.request.api_method.id) && r.body.empty?
+
+    JSON.parse(r.body)
+
+
+  end
+end
---
+++ pubsub/pubsub_client.rb
@@ -0,0 +1,168 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'yaml'
+require 'google/api_client'
+require 'json'
+require 'yaml'
+require 'hash_params'
+require 'pry'
+require 'base64'
+
+require_relative 'google_api_client'
+
+config = YAML::load_file(File.join(__dir__, 'pubsub.local.yml'))
+
+class PubSubTopic
+  def initialize(client, topic_name)
+    @client    = client
+    @topic_name=topic_name
+  end
+
+  def subscription_list
+  end
+
+  def list
+    r= @client.execute('topics.list', {'query' => "cloud.googleapis.com/project in (/projects/#{@project_id})"})
+    r['topic'].map { |t| t['name'].split('/')[-1] }
+  end
+
+  def find_or_create(topic_name)
+    topics = topic_list
+    create_topic(topic_name) unless topics.include?(topic_name)
+  end
+end
+
+class PubSub
+
+  def initialize(client, project_id)
+    @client     =client
+    @project_id = project_id
+    @client.discover('pubsub', 'v1beta1')
+  end
+
+  def topic_list
+    r= @client.execute('topics.list', {'query' => "cloud.googleapis.com/project in (/projects/#{@project_id})"})
+    r['topic'].map { |t| t['name'].split('/')[-1] }
+  end
+
+  def subscription_list(topic_name = nil)
+    s= if topic_name
+         "pubsub.googleapis.com/topic in (/topics/#{@project_id}/#{topic_name})"
+       else
+         "cloud.googleapis.com/project in (/projects/#{@project_id})"
+       end
+
+    r = @client.execute('subscriptions.list', {"query" => s})
+    r['subscription'].map { |t| t['name'].split('/')[-1] }
+  end
+
+  def find_or_create_topic(topic_name)
+    topics = topic_list
+    create_topic(topic_name) unless topics.include?(topic_name)
+  end
+
+  def create_topic(topic_name)
+    @client.execute 'topics.create', {}, topic_resource(topic_name)
+  end
+
+  def get_topic(topic_name)
+    @client.execute('topics.get', {"topic" => topic_path(topic_name)})
+  end
+
+  def get_subscription(subscription_name)
+    @client.execute('subscriptions.get', {"subscription" => subscription_path(subscription_name)})
+  end
+
+
+  def publish(topic_name, data, labels=nil)
+    #Message payloads should be Base64-encoded, and can be a maximum of 7 MB before encoding.
+    #  (The client libraries provide methods for Base64-encoding data.)
+
+
+    h                     = {
+        "topic"   => topic_path(topic_name),
+        "message" => {
+            "data" => Base64.encode64(data)
+        }
+    }
+    h["message"]["label"] = Array(labels) if labels
+    @client.execute('topics.publish', {}, h)
+
+  end
+
+  def create_subscription(topic_name, subscription_name, ack_deadline_in_seconds, push_endpoint=nil)
+    body               = {
+        "name"               => subscription_path(subscription_name),
+        "topic"              => topic_path(topic_name),
+
+        "ackDeadlineSeconds" => ack_deadline_in_seconds
+    }
+    body["pushConfig"] = {"pushEndpoint" => push_endpoint} if push_endpoint
+
+    @client.execute('subscriptions.create', {}, body)
+  end
+
+  def find_or_create_subscription(topic_name, subscription_name, ack_deadline_in_seconds, push_endpoint=nil)
+    subs=subscription_list(topic_name)
+    create_subscription(topic_name, subscription_name, 600) unless subs.include?(subscription_name)
+  end
+
+  def pull_from_subscription(subscription_name, return_immediately = true)
+    #returns an array with key and data
+    body = {"subscription" => subscription_path(subscription_name), "returnImmediately" => return_immediately}
+    r    = @client.execute('subscriptions.pull', {}, body)
+    [r['ackId'], Base64.decode64(r['pubsubEvent']['message']['data'])]
+
+  end
+
+  def acknowledge(subscription_name, ack_id)
+    #this API takes an array in the ackid but the documentation says it's a single id
+    body= {
+        "subscription" => subscription_path(subscription_name),
+        "ackId"        => Array(ack_id)
+    }
+    @client.execute('subscriptions.acknowledge', {}, body)
+  end
+
+  private
+  def topic_path(topic_name)
+    "/topics/#{@project_id}/#{topic_name}"
+  end
+
+  def subscription_path(subscription_name)
+    "/subscriptions/#{@project_id}/#{subscription_name}"
+  end
+
+  def topic_resource(topic_name)
+    {"name" => topic_path}
+  end
+
+
+end
+
+
+client = GoogleApiClient.new(config)
+@pubsub=PubSub.new(client, config['project_id'])
+
+
+binding.pry
+topic_name        = 'test-topic'
+subscription_name = 'test-subscription'
+
+@pubsub.find_or_create_topic(topic_name)
+@pubsub.find_or_create_subscription(topic_name, subscription_name, 600)
+
+
+response = @pubsub.publish('test-topic', "This is a message posted to test_topic")
+
+10.times do
+
+
+  ack_id, data = @pubsub.pull_from_subscription('test-subscription')
+  binding.pry
+  ack          = @pubsub.acknowledge(subscription_name, ack_id)
+end
+
+binding.pry
+
